df66a43b6bcba680d4111aa977d49dfc049434e5
Arifakash16 <mdarifulislam6898@gmail.com>
2024-09-28 00:38:55 +0600

    Revert "Completed Product Catagory crud operation except it's view page. To check this crud I am using postman rest client."
    This reverts commit 0bc4ac3b96a84a5c41d20400b18c517b72dee1c1.

--- BasicInventoryManagementSystem/Repository/IRepository/IRegistrationRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BasicInventoryManagementSystem.Models;
-
-namespace BasicInventoryManagementSystem.Repository.IRepository
-{
-    public interface IRegistrationRepository
-    {
-        public void RegisterUserToDb(Registration registration);
-    }
-}
--- BasicInventoryManagementSystem/Service/IService/ILoginService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BasicInventoryManagementSystem.Service.IService
-{
-    public interface ILoginService
-    {
-        public bool LoginUser(string email, string password);
-    }
-}
